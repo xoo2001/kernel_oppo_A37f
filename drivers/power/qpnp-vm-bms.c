2358df4cf3278b84a52f846b25bddd1dee3f7f83
Rakesh Bhagat <rakesh.5615rb@gmail.com>
2021-06-15 17:11:55 +0530

    drivers:power: oppo A37f (15399) changes

--- drivers/power/qpnp-vm-bms.c
+++ drivers/power/qpnp-vm-bms.c
@@ -3567,7 +3567,91 @@ static int set_battery_data(struct qpnp_bms_chip *chip)
 				return -EINVAL;
 			}
 		}
-	} else {
+	}else if(is_project(OPPO_15399)){
+		#if 1
+		rc = opchg_get_bq2022_manufacture_id();
+		if(rc < 0){
+			pr_err("%s get bq2022 manu id fail\n",__func__);
+			return -EPROBE_DEFER;
+		} 
+		else if(rc == BATTERY_2420MAH_SDI)
+		{
+			node = of_find_node_by_name(chip->spmi->dev.of_node,
+						"qcom,battery-data-sdi");
+			if (!node) {
+				pr_err("No available atl batterydata\n");
+				return -EINVAL;
+			}
+			else
+			{
+				pr_err("available atl batterydata\n");
+			}
+		}
+		else if(rc == BATTERY_2550MAH_SDI)
+		{
+			node = of_find_node_by_name(chip->spmi->dev.of_node,
+						"qcom,battery-data-high-sdi");
+			if (!node) {
+				pr_err("No available high-sdi batterydata\n");
+				return -EINVAL;
+			}
+			else
+			{
+				pr_err("available high-sdi batterydata\n");
+			}
+		}
+		else if(rc == BATTERY_2550MAH_LG)
+		{
+			node = of_find_node_by_name(chip->spmi->dev.of_node,
+						"qcom,battery-data-high-lg");
+			if (!node) {
+				pr_err("No available high-lg batterydata\n");
+				return -EINVAL;
+			}
+			else
+			{
+				pr_err("available high-lg batterydata\n");
+			}
+		} 
+		else if(rc == BATTERY_2550MAH_ATL)
+		{
+			node = of_find_node_by_name(chip->spmi->dev.of_node,
+						"qcom,battery-data-high-atl");
+			if (!node) {
+				pr_err("No available high-atl batterydata\n");
+				return -EINVAL;
+			}
+			else
+			{
+				pr_err("available high-atl batterydata\n");
+			}
+		}
+		else if(rc == BATTERY_2420MAH_ATL)
+		{
+			node = of_find_node_by_name(chip->spmi->dev.of_node,
+						"qcom,battery-data-atl");
+			if (!node) {
+				pr_err("No available atl batterydata\n");
+				return -EINVAL;
+			}
+			else
+			{
+				pr_err("available atl batterydata\n");
+			}
+		} 
+		#else
+		node = of_find_node_by_name(chip->spmi->dev.of_node,
+					"qcom,battery-data-atl");
+		if (!node) {
+			pr_err("No available batterydata\n");
+			return -EINVAL;
+		}
+		else
+		{
+			pr_err("available atl batterydata\n");
+		}
+		#endif
+	}else {
 		node = of_find_node_by_name(chip->spmi->dev.of_node,
 					"qcom,battery-data");
 		if (!node) {
