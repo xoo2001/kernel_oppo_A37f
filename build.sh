f1108281b4a7b8049bb32cd5a61d97ecd2857a4b
Rakesh Bhagat <rakesh.5615rb@gmail.com>
2021-06-17 21:36:09 +0530

    Build script changes

--- build.sh
+++ build.sh
@@ -1,42 +1,42 @@
 #!/bin/bash
-
+BUILD_START=$(date +"%s")
 tcdir=${HOME}/android/TOOLS/GCC
 
-virtualenv2 venv
-source venv/bin/activate
-
 [ -d "out" ] && rm -rf out && mkdir -p out || mkdir -p out
-[ -d "venv" ] && rm -rf venv && mkdir -p venv || mkdir -p venv
-
-make O=out ARCH=arm64 lineageos_a37f_defconfig
 
 [ -d $tcdir ] && \
 echo "ARM64 TC Present." || \
 echo "ARM64 TC Not Present. Downloading..." | \
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_aarch64_aarch64-linux-android-4.9 $tcdir/los-4.9-64
+git clone https://bitbucket.org/UBERTC/aarch64-linux-android-4.9.git $tcdir/uber
 
-[ -d $tcdir ] && \
-echo "ARM32 TC Present." || \
-echo "ARM32 TC Not Present. Downloading..." | \
-git clone --depth=1 https://github.com/LineageOS/android_prebuilts_gcc_linux-x86_arm_arm-linux-androideabi-4.9 $tcdir/los-4.9-32
+echo -e "\e[1;31m***********************************************"
+echo "          Compiling A37f Kernel          "
+echo -e "***********************************************\e[0m"
 
-PATH="$tcdir/los-4.9-64/bin:$tcdir/los-4.9-32/bin:${PATH}" \
+make O=out ARCH=arm64 lineageos_a37f_defconfig
+
+PATH="$tcdir/uber/bin:${PATH}" \
 make    O=out \
         ARCH=arm64 \
-        CC="ccache $tcdir/los-4.9-64/bin/aarch64-linux-android-gcc" \
+        CC="ccache $tcdir/uber/bin/aarch64-linux-android-gcc" \
         CROSS_COMPILE=aarch64-linux-android- \
-        CROSS_COMPILE_ARM32=arm-linux-androideabi- \
         CONFIG_NO_ERROR_ON_MISMATCH=y \
         CONFIG_DEBUG_SECTION_MISMATCH=y \
         -j$(nproc --all) || exit
 
-cc anykernel3/dtbtool.c -o out/arch/arm64/boot/dts/dtbtool
-cd out/arch/arm64/boot/dts
-./dtbtool -s 2048 -o dt.img
-cd ../../../../..
+echo -e "\e[1;32m**** Copying Image ****\e[0m"
+cp out/arch/arm64/boot/Image anykernel3
 
-fmt=`date +%d\.%m\.%Y_%H\:%M\:%S`
-pre=A37F
-cp out/arch/arm64/boot/dts/dt.img anykernel3 && cp out/arch/arm64/boot/Image anykernel3 && cd anykernel3
-zip -r ${pre}_KERNEL_${fmt}.zip . -x 'LICENSE' 'README.md' 'dtbtool.c'
-mv *.zip ../out
+cc anykernel3/dtbtool.c -o out/arch/arm64/boot/dts/dtbtool
+echo -e "\e[1;35m*******Building DTB********\e[0m"
+( cd out/arch/arm64/boot/dts; ./dtbtool -v -s 2048 -o dt.img )
+echo -e "\e[1;33m**** Copying dtb ****\e[0m"
+( cp out/arch/arm64/boot/dts/dt.img anykernel3 )
+
+echo -e "\e[1;36m**** Time to zip up! ****\e[0m"
+( cd anykernel3; zip -r ../out/A37F_KERNEL_`date +%d\.%m\.%Y_%H\:%M\:%S`.zip . -x 'LICENSE' 'README.md' 'dtbtool.c' )
+
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+echo -e "\e[1;32m**** Enjoy New Shit!! ****\e[0m"
+echo -e "\e[1;42mBuild completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.\e[0m"
