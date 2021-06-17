kumajaya/android_kernel_cyanogen_msm8916

input: misc: Don't probe lis3dh in recovery mode
* Hack to stop lis3dh as input device in recovery mode

Change-Id: I42416c84e12b9d27e45a6f50bf7856adde0ad414

drivers/input/misc/lis3dh_acc.c
@@ -2206,12 +2206,31 @@ static int lis3dh_parse_dt(struct device *dev,
 }
 #endif
 
+#ifdef CONFIG_MACH_PD1510
+unsigned int recoverymode = 0;
+static int lis3dh_acc_get_recoverymode(char *str)
+{
+	if (strncmp(str, "1", 1) == 0)
+		recoverymode = 1;
+
+	return recoverymode;
+}
+__setup("recoverymode=", lis3dh_acc_get_recoverymode);
+#endif
+
 static int lis3dh_acc_probe(struct i2c_client *client,
 		const struct i2c_device_id *id)
 {
 
 	struct lis3dh_acc_data *acc;
 	int err = -1;
+
+#ifdef CONFIG_MACH_PD1510
+	if (recoverymode) {
+		pr_info("%s: Don't probe in recovery mode\n", __func__);
+		return err;
+	}
+#endif
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		dev_err(&client->dev, "client not i2c capable\n");

xoo2001/kernel_oppo_A37f

drivers/input/misc/lis3dh_acc.c
@@ -2441,6 +2441,18 @@ static const struct attribute *lis3dh_attrs[] =
 };
 
 static struct dev_ftm lis3dh_ftm;
+
+#ifdef CONFIG_MACH_OPPO
+unsigned int recoverymode = 0;
+static int lis3dh_acc_get_recoverymode(char *str)
+{
+	if (strncmp(str, "1", 1) == 0)
+		recoverymode = 1;
+
+	return recoverymode;
+}
+__setup("recoverymode=", lis3dh_acc_get_recoverymode);
+#endif
 #endif
 
 static int lis3dh_acc_probe(struct i2c_client *client,
@@ -2449,6 +2461,13 @@ static int lis3dh_acc_probe(struct i2c_client *client,
 
 	struct lis3dh_acc_data *acc;
 	int err = -1;
+
+#ifdef CONFIG_MACH_OPPO
+	if (recoverymode) {
+		pr_info("%s: Don't probe in recovery mode\n", __func__);
+		return err;
+	}
+#endif
 
 	if (!i2c_check_functionality(client->adapter, I2C_FUNC_I2C)) {
 		dev_err(&client->dev, "client not i2c capable\n");